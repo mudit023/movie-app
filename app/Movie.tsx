5659cf23aeeebe817a0001848da263fc6ff87eb6
Mudit Mishra <Mudit.12016298@lpu.in>
2023-07-05 12:03:33 +0530

    added grid structure

--- app/Movie.tsx
+++ app/Movie.tsx
@@ -1,23 +1,23 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
-interface movieProps{
-  title: string,
-  about: string,
-  poster_path: string,
-  rating: number,
-  release_date: string,
-  id: number
+interface movieProps {
+  title: string;
+  about: string;
+  poster_path: string;
+  rating: number;
+  release_date: string;
+  id: number;
 }
 
 function Movie(props: movieProps) {
-  const IMAGE_PATH:string = `https://image.tmdb.org/t/p/original${props.poster_path}`
+  const IMAGE_PATH: string = `https://image.tmdb.org/t/p/original${props.poster_path}`;
   return (
-    <>
+    <div>
       <h1>{props.title}</h1>
       <div>
         <Link href={"/abc"}>
-          <Image src={IMAGE_PATH} alt={props.title} width={400} height={400}/>
+          <Image src={IMAGE_PATH} alt={props.title} width={400} height={400} />
         </Link>
         <div>
           <p>{props.about}</p>
@@ -27,8 +27,8 @@ function Movie(props: movieProps) {
           </div>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Movie
+export default Movie;
--- app/page.tsx
+++ app/page.tsx
@@ -19,20 +19,22 @@ export default async function Home() {
   // console.log(res.results);
 
   return (
-    <main className="">
-      {res?.results?.map((item: movieResult, idx: number) => {
-        return (
-          <Movie
-            title={item?.title}
-            about={item?.overview}
-            poster_path={item?.poster_path}
-            rating={item?.vote_average}
-            release_date={item?.release_date}
-            key={idx}
-            id={item?.id}
-          />
-        );
-      })}
+    <main className="mt-16 md:px-10 px-5">
+      <div className="grid gap-16 grid-cols-fluid">
+        {res?.results?.map((item: movieResult, idx: number) => {
+          return (
+            <Movie
+              title={item?.title}
+              about={item?.overview}
+              poster_path={item?.poster_path}
+              rating={item?.vote_average}
+              release_date={item?.release_date}
+              key={idx}
+              id={item?.id}
+            />
+          );
+        })}
+      </div>
     </main>
   );
 }
